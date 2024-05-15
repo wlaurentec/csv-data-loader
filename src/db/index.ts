fb97819f1ed404711857232f0cc6f0b6c16f48e7
Walter Laurente <wlaurente7@gmail.com>
2024-05-14 20:54:30 -0500

    Create db

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const { Pool } = pg;
+
+const dbConfig = {
+  host: "localhost",
+  port: 5432,
+  database: "data-loader",
+  user: process.env["DB_USER"], //Tu usuario
+  password: process.env["DB_PASS"], //Contraseña
+};
+export const pool = new Pool(dbConfig);
+
+export const query = (text: string, params?: (string | number | boolean)[]) => {
+  return pool.query(text, params);
+};
---
+++ src/db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+	email varchar(255) NOT NULL UNIQUE,
+	password varchar(60) NOT NULL
+    age integer NULL CHECK (age > 0),
+    role varchar(255) NOT default 'user',
+);
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ 
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+ 
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
